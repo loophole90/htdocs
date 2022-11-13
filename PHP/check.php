1ff277e23c19d14b7c4494a62a2bd095c0dbc3aa
loophole90 <45695552+loophole90@users.noreply.github.com>
2022-11-13 23:55:39 +0200

    Update check.php

--- PHP/check.php
+++ PHP/check.php
@@ -3,13 +3,13 @@
 	$gmail = filter_var(trim($_POST['gmail']), FILTER_SANITIZE_STRING);
 	$pass = filter_var(trim($_POST['pass']), FILTER_SANITIZE_STRING);
 	if (mb_strlen($login) < 4 || mb_strlen($login) > 20) {
-		echo "Недопустимая длина логина";
+		echo "Недопустимая довжина логина";
 		exit();
 	}else if (mb_strlen($gmail) < 1 || mb_strlen($gmail) > 30) {
-		echo "Недопустимая длина електронной почты";
+		echo "Недопустимая довжина електронной почты";
 		exit();
 	}else if (mb_strlen($pass) < 8 || mb_strlen($pass) > 16) {
-		echo "Недопустимая длина пароля";
+		echo "Недопустимая довжина пароля";
 		exit();
 	}
 	$pass = md5($pass."janskdngsdhj");
@@ -18,17 +18,16 @@
 	$mysql->query("INSERT INTO `users` (`login`, `gmail`, `pass`) VALUES('$login', '$gmail', '$pass')");
 	$login2 = $mysql->query("SELECT * FROM `users`  WHERE `login` = '$login'");
 	$user = $login2->fetch_assoc();
-	if(count($user) != 0) {
-		echo "Такой пользователь уже занят.";
-		exit();
-	}
 	$gmail2 = $mysql->query("SELECT * FROM `users`  WHERE `gmail` = '$gmail'");
 	$user2 = $gmail2->fetch_assoc();
-	if(count($user2) != 0) {
-		echo "Такая електронная почта уже занята.";
+	if(count($user) != 0) {
+		echo "Такий логін вже занятий.";
 		exit();
 	}
-
+	if(count($user2) != 0) {
+		echo "Така електронная почта вже занята.";
+		exit();
+	}
 	$mysql->close();
 
 	header('Location: /')
