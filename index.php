1b44b98a6e5419515d2b7b7b57de1e083bbb7aad
loophole90 <45695552+loophole90@users.noreply.github.com>
2022-11-13 23:57:12 +0200

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="">
-    <title>Форма регистрации</title>
+    <title>Форма реєстрації</title>
 
   </head>
   <body>
@@ -18,14 +18,17 @@
       ?>
       <div class="row">
         <div class="col">
-          <h1>Форма регистрации</h1>
+          <h1>Форма реєстрації</h1>
           <form action="PHP/check.php" method="post">
             <input  type="text" class="form-control" name="login" id="login" placeholder="Введіть логін"><br>
 
             <input  type="email" class="form-control" name="gmail" id="gmail" placeholder="Введіть свою електронну пошту"><br>
 
             <input  type="password" class="form-control" name="pass" id="pass" placeholder="Введіть пароль"><br>
-            <button type="submin" >Зарегистрировать</button>
+
+            <button type="submin" >Зареєструватись</button>
+            <a title="GitHub" rel="noreferer" class="btn socials-item btn-github" href="/auth/github/login/?process=login">
+  </a>
           </form>
         </div>
         <h1>Форма авторизации</h1>
@@ -34,15 +37,16 @@
           <input  type="text" class="form-control" name="login" id="login" placeholder="Введіть логін"><br>
 
           <input  type="password" class="form-control" name="pass" id="pass" placeholder="Введіть пароль"><br>
-          <button type="submin" >Авторизоваться</button>
-          <h6>Все данные содержаються всего 30 дней,<br> после чего нужно заново авторизоруваться.</h6>
+          <button type="submin" >Авторизуватись</button>
+          <h6>Всі данні тримаються тільки 30 днів,<br> після цього треба знову авторизуватися.</h6>
         </form>
       </div>
       <?php else: ?>
-        <p><?=$_COOKIE['user']?>, Чтобы перейти на сайт нажмите на кнопу снизу.</p>
-        <form action="site.php">
-        <button>Перейти</button>
-      </form>
+        <!-- <?=$_COOKIE['user']?> чтобы отобразить имя аккаунта  -->
+        <p>Щоб вийти з аккаунту натисніть на кнопку знизу.</p>
+        <form action="exit.php">
+          <button>Вийти</button>
+        </form>
       <?php endif; ?>
     </div>
   </body>
