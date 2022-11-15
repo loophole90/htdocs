bbf38df98561ea15fe4ed29dae71ed061140e269
loophole90 <45695552+loophole90@users.noreply.github.com>
2022-11-15 21:16:40 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,53 +1,57 @@
 <!DOCTYPE html>
-<html lang="ru">
-  <head>
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="">
-    <title>Форма реєстрації</title>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="CSS/Style2.css">
+	<link href="https://fonts.googleapis.com/css?famili=Kaushan+Script|Montserrat:400,700&amp;subset=cyrillic-ext" rel="stylesheet">
+	<title></title>
+</head>
+<?php
+if ($_COOKIE['user'] == ''):
+?>
+<body class="background">
+	<header class="header">
+		<div class="conteiner">
+			<div class="header_inner">
+				<div class="header__logo">MoGo</div>
+				<nav>
+					<a class="nav__link" href="#">ABOUT</a>
+					<a class="nav__link" href="#">SERVICE</a>
+					<a class="nav__link" href="#">WORK</a>
+					<a class="nav__link" href="#">BLOG</a>
+					<a class="nav__link" href="#">CONTACT</a>
+					<a class="nav__link" href="registration.php">REGISTRATION/AUTHORIZATION</a>
+				</nav>
 
-  </head>
-  <body>
+			</div>
+		</div>
+	<?php else: ?>
+		<!-- <?=$_COOKIE['user']?> чтобы отобразить имя аккаунта  -->
 
-    <div class="container mt-4">
+	</form>
+	<body class="background">
+		<header class="header">
+			<div class="conteiner">
+				<div class="header_inner">
+					<div class="header__logo">MoGo</div>
+					<nav>
+						<a class="nav__link" href="#">ABOUT</a>
+						<a class="nav__link" href="#">SERVICE</a>
+						<a class="nav__link" href="#">WORK</a>
+						<a class="nav__link" href="#">BLOG</a>
+						<a class="nav__link" href="#">CONTACT</a>
+						<a class="nav__link" href="exit.php">EXIT</a>
 
-      <?php
-      if ($_COOKIE['user'] == ''):
-      ?>
-      <div class="row">
-        <div class="col">
-          <h1>Форма реєстрації</h1>
-          <form action="PHP/check.php" method="post">
-            <input  type="text" class="form-control" name="login" id="login" placeholder="Введіть логін"><br>
+					</nav>
 
-            <input  type="email" class="form-control" name="gmail" id="gmail" placeholder="Введіть свою електронну пошту"><br>
-
-            <input  type="password" class="form-control" name="pass" id="pass" placeholder="Введіть пароль"><br>
-
-            <button type="submin" >Зареєструватись</button>
-            <a title="GitHub" rel="noreferer" class="btn socials-item btn-github" href="/auth/github/login/?process=login">
-  </a>
-          </form>
-        </div>
-        <h1>Форма авторизации</h1>
-        <form action="PHP/auth.php" method="post">
-
-          <input  type="text" class="form-control" name="login" id="login" placeholder="Введіть логін"><br>
-
-          <input  type="password" class="form-control" name="pass" id="pass" placeholder="Введіть пароль"><br>
-          <button type="submin" >Авторизуватись</button>
-          <h6>Всі данні тримаються тільки 30 днів,<br> після цього треба знову авторизуватися.</h6>
-        </form>
-      </div>
-      <?php else: ?>
-        <!-- <?=$_COOKIE['user']?> чтобы отобразить имя аккаунта  -->
-        <p>Щоб вийти з аккаунту натисніть на кнопку знизу.</p>
-        <form action="exit.php">
-          <button>Вийти</button>
-        </form>
-      <?php endif; ?>
-    </div>
-  </body>
+				</div>
+			</div>
+	<?php endif; ?>
+	</header>
+	<div class="intro">
+		<div class="conteiner">
+		</div>
+	</div>
+</body>
 </html>
